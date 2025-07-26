6df0ce487bd74319c3c05d34cbaec29bb9165b4b
soyesenna <kjy915875@gmail.com>
2025-07-26 23:01:35 +0900

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { cli, define } from "gunshi";
+import add from "./commands/add";
+
+const subCommands = new Map();
+subCommands.set("add", add);
+
+const mainCommand = define({
+  name: "upstage-ssh",
+  description: "Manage ssh settings automatically",
+  run: () => {
+    console.log("upstage-ssh");
+  },
+});
+
+cli(process.argv.slice(2), mainCommand, {
+  name: "upstage-ssh",
+  version: "0.0.1",
+  description: "Manage ssh settings automatically",
+  subCommands,
+});
